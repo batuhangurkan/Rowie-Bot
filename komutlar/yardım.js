a8f4b16d4ffd3a0d2bbd815fdd769da761b67896
Glitch (hello-express) <none>
2020-05-23 18:59:50 +0000

    🍈🙅 Checkpoint ./komutlar/yardım.js:5191335/40 ./komutlar/reklamengel.js:5191335/1007 ./boşaltyapı.js:5191335/1783

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,10 +7,10 @@ exports.run = (client, message, args) => {if(db.fetch(`bakim`)) return message.c
     .setColor('RANDOM')
     .setAuthor(`${client.user.username} `, client.user.avatarURL) 
       .setDescription('**!yardım** ile yardım alabilirsiniz.\n örnek komut kullanımı: **!küfür-engel aç**.\nbotu davet etmek için **!davet**')
-      .addField('** Komutlar (21)**', `Herkesin Kullanabileceği Standart Komutlar. \n` +  '`botbilgi`, `ping`, `başvuru`, `lyrics`, `istatistik`, `döviz`, `roller`, `rrenk`, `sunucubilgi`, `yapımcım`, `yaz`, `yetkililer`,`davet`,`şifre`, `havadurumu`, `öneri`, `avatar`, `bitcoin`, `seslimesaj`, `ailemiz`, `yetkilerim`')
+      .addField('** Komutlar (22)**', `Herkesin Kullanabileceği Standart Komutlar. \n` +  '`botbilgi`, `ping`, `başvuru`, `lyrics`, `istatistik`, `döviz`, `roller`, `rrenk`, `sunucubilgi`, `yapımcım`, `yaz`, `yetkililer`,`davet`,`şifre`, `havadurumu`, `öneri`, `avatar`, `bitcoin`, `seslimesaj`, `ailemiz`, `yetkilerim`, `afk`')
       .addField('** Eğlence (27)**',   `Herkes İçin Kullanılabilecek Eğlence Komutları. \n` + '`animeavatar`, `aşkımı-ölç`, `ateşet`, `randomhabbo`, `tokyo-ghoul`, `sinirli`, `kedi`, `köpek`, `berserk`, `mcskin`, `öp`, `sarıl`, `efkarölçer`, `emojiyaz`, `hesapla`, `yazı-banner`, `clyde`, `csgokasa`, `bjkefekt`, `gsefekt`,  `kanna`, `sonic`, `balıktut`, `8ball`, `meme`, `muzum`, `ph`')
       .addField('** Bilgi (16)**',`Herkes İçin Kullanılıcak Bilgi Alma Komutları. \n` +  '`spotify`, `lolbilgi`, `instagram`, `youtubebilgi`, `steamfiyat`, `twitch`, `vikipedi`, `npm`, `discordbotlist`, `yılbaşı`, `diziara`, `woodie`, `google`, `corona`, `twitter`, `gifara`')
-      .addField('** Moderasyon (18)**',`Yetkililer İçin Moderasyon Komutları Bölümü . \n` +  '`küfür-engel`, `reklam-tara`, `kick`,`mute`,`sil(gelişmiş)`, `temizle`, `votekick`, `kilit(kanal kilitler)`, `duyuru`, `ban`, `unban`, `anket`, `banlananlar`, `yavaşmod`, `ıd`, `reklamfiltresi`, `hazır-kanallar`, `panel`')
+      .addField('** Moderasyon (18)**',`Yetkililer İçin Moderasyon Komutları Bölümü . \n` +  '`küfür-engel`, `reklam-engelle`, `kick`,`mute`,`sil(gelişmiş)`, `temizle`, `votekick`, `kilit(kanal kilitler)`, `duyuru`, `ban`, `unban`, `anket`, `banlananlar`, `yavaşmod`, `ıd`, `reklamfiltresi`, `hazır-kanallar`, `panel`')
       .addField('** NSFW Komutları (2)**',`+18 İçeriktir. Sadece NSFW Kanalında Bu Komutlar Çalışır. \n` +  '`nsfw`, `nsfw-gif`')
       .addField('** Bot Sahibine Özel Komutlar (7)**',`Bot sahibinin kullanabiliceği komutlar. \n` +  '`dnd`, `idle`, `load`, `unload`, `eval`, `reboot`, `bakım` ')
     
