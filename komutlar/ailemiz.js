2924003578f27d4aeadd308ab8b22026549b658d
Glitch (hello-express) <none>
2020-05-26 00:02:13 +0000

    🎿🎪 Checkpoint ./boşaltyapı.js:5191335/5 ./komutlar/ailemiz.js:5191335/10

--- komutlar/ailemiz.js
+++ komutlar/ailemiz.js
@@ -9,7 +9,7 @@ exports.run = (bot, message) => {if(db.fetch(`bakim`)) return message.channel.se
       embed.addField(`**${guild.name}** - ÜYE SAYISI : **${guild.memberCount}**`, guild.id);
       embed.setColor('#D97634')
       embed.setTitle('Ailemiz')
-      embed.setDescription(`Büyük bir ailedeyiz !. Ailemde **${bot.guilds.size}** kadar sunucu var ! (RowieBot`)
+      embed.setDescription(`Büyük bir ailedeyiz !. Ailemde **${bot.guilds.size}** kadar sunucu var ! (GultsBot`)
       message.delete();
     }
     message.channel.send({embed: embed});
