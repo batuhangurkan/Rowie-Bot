45d49c755fb8f05575c0f4e79ecdc157f9da75bb
Glitch (hello-express) <none>
2020-06-05 10:05:26 +0000

    👓🍈 Checkpoint ./package.json:5191335/8020

--- util/eventLoader.js
+++
@@ -1,9 +0,0 @@
-const reqEvent = (event) => require(`../events/${event}`);
-module.exports = client => {
-  client.on('ready', () => reqEvent('ready')(client));    
-  client.on('message', reqEvent('message'));
-  client.on('guildMemberAdd', () => reqEvent('giriş'));
-  client.on('guildMemberRemove', reqEvent('çıkış'));
-
-};
-
