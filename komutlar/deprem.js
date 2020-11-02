bc83b99a37abc7941a8b17fed49b0c25a395a4b5
Glitch (gultsbot1) <none>
2020-11-02 19:06:44 +0000

    🌌🏮 Checkpoint ./komutlar/deprem.js:5691370/616

--- komutlar/deprem.js
+++ komutlar/deprem.js
@@ -7,16 +7,18 @@ exports.run = (client, msg, args) => {
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     let deprem = JSON.parse(body);
     console.log(deprem);
-    msg.channel.sendEmber(new Discord.RichEmbed()
+    msg.channel.sendEmbed(new Discord.RichEmbed()
       .setColor("BLUE")
       .setTitle(`${deprem.result[0].title}`)
-      .addField("", "" , true)
+      .addField("Lng:", `${deprem.result[0].lng}` , true)
+      .addField("Lat:", `${deprem.result[0].lat}` , true)
+      .addField("Mag:", `${deprem.result[0].mag}` , true)
+      .addField("Location:", `${deprem.result[0].lokasyon}` , false)
+      .addField("Depth:", `${deprem.result[0].depth}` , true)                    
       .setFooter(`${deprem.result[0].date}`)
                         
                          
     );
-    
-    msg.channel.send("Korona "+deprem . result[0]. title);
   });
   
 };
