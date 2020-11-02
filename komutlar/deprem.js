a72e283a8bdae0d5df904bdca51812635693e5eb
Glitch (gultsbot1) <none>
2020-11-02 18:54:58 +0000

    🙋👯 Checkpoint ./komutlar/deprem.js:5691370/1006

--- komutlar/deprem.js
+++ komutlar/deprem.js
@@ -2,13 +2,21 @@ const Discord = require('discord.js');
 const request = require('request');
 
 exports.run = (client, msg, args) => {
-  request(`https://corona.lmao.ninja/v2/all`, function(error,response,body){
+  request(`https://api.orhanaydogdu.com.tr/deprem/live.php?limit=1`, function(error,response,body){
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
     let deprem = JSON.parse(body);
     console.log(deprem);
-    msg.channel.send("Hello"+stats . title);
+    msg.channel.sendEmber(new Discord.RichEmbed()
+      .setColor("BLUE")
+      .setTitle(`${deprem.result[0].title}`)
+      .addField("", "" , true)
+      .setFooter(`${deprem.result[0].date}`)
+                        
+                         
+    );
+    
+    msg.channel.send("Korona "+deprem . result[0]. title);
   });
   
 };
